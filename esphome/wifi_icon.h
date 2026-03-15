4460c438d06605f3b2627738ad417aec78b4f649
Anoop Dhiman <dhiman.anoop09@gmail.com>
2026-03-15 15:54:20 +0530

    wifi status

---
+++ esphome/wifi_icon.h
@@ -0,0 +1,14 @@
+const char * wifi_icon(float wifi_strength) {
+    if (isnan(wifi_strength))
+			return "󰤮"; // No-wifi
+	else if (wifi_strength < 10)
+			return "󰤯";  // low-wifi
+	else if (wifi_strength < 30)
+			return "󰤟"; // wifi-1
+	else if (wifi_strength < 50)
+			return "󰤢"; // wifi-2
+	else if (wifi_strength < 75)
+			return "󰤥"; // wifi-3
+	else
+			return "󰤨"; // wifi-4
+}
